77312f2d838fd575e8a52a117a4856ef654af3d6
Denzel <denzelmok@gmail.com>
2020-12-18 03:13:29 +1100

    Implemented scrapping of all movie links Scrapping of all information complete Creation of csv file Cleaned up file Bugfixes

--- python-imdb-scrapper/python-imdb-scrapper.py
+++ python-imdb-scrapper/python-imdb-scrapper.py
@@ -12,7 +12,7 @@ votes = []
 metascores = []
 reviews = []
 critics = []
-popularitys = []
+#popularitys = [] <- too many bugs
 countrys = []
 worldwide_grosses = []
 movie_classes = []
@@ -27,9 +27,7 @@ for link in soup.find_all('a'):
         urls.append(link.get('href'))
 
 # store data from each link
-#for url in urls:
-for i in range(10):
-    url = urls[i]
+for url in urls:
     page = requests.get('https://www.imdb.com/' + url)
     soup = BeautifulSoup(page.text, 'html.parser')
 
@@ -54,8 +52,11 @@ for i in range(10):
     votes.append(vote)
 
     # store metascores
-    metascore = soup.find('div', class_="metacriticScore").contents
-    metascore = sub(r"\D", "", str(metascore))
+    if soup.find('div', class_="metacriticScore") is not None:
+        metascore = soup.find('div', class_="metacriticScore").contents
+        metascore = sub(r"\D", "", str(metascore))
+    else:
+        metascore = None
     metascores.append(metascore)
 
     # store review number
@@ -69,33 +70,43 @@ for i in range(10):
     critics.append(critic)
 
     # store popularity
-    popularity = soup.find_all('div', class_="titleReviewBarSubItem")[2].span.text.split()[0]
-    popularitys.append(popularity)
+    #if len(soup.find_all('div', class_="titleReviewBarSubItem")) > 1:
+    #    popularity = soup.find_all('div', class_="titleReviewBarSubItem")[2].span.text.split()[0]
+    #elif soup.find('div', class_="titleReviewBarSubItem") is None:
+    #    popularity = None
+    #else:
+    #    popularity = soup.find('div', class_="titleReviewBarSubItem").span.text.split()[0]
+    #popularitys.append(popularity)
 
     # store country
     country = soup.find('h4', class_="inline", text="Country:").next_element.next_element.next_element.text
     countrys.append(country)
 
     # store worldwide gross
-    wwg = soup.find('h4', class_="inline", text="Cumulative Worldwide Gross:").next_element.next_element.strip()
+    if soup.find('h4', class_="inline", text="Cumulative Worldwide Gross:") is not None:
+        wwg = soup.find('h4', class_="inline", text="Cumulative Worldwide Gross:").next_element.next_element.strip()
+    else:
+        wwg = None
     worldwide_grosses.append(wwg)
 
     # store movie ratings
     movie_class = soup.find('h4', class_="inline", text="Certificate:").next_element.next_element.next_element.text
+    if len(movie_class) > 5:
+        movie_class = None
     movie_classes.append(movie_class)
 
 # create the dataset
-movies = pd.DataFrame({'movie':titles,
-                       'year':years,
-                       'time_minute':times,
-                       'imdb_rating':imdb_ratings,
-                       'imdb_voters':votes,
-                       'metascore':metascores,
-                       'reviews':reviews,
-                       'critics':critics,
-                       'popularity':popularitys,
+movies = pd.DataFrame({'movie': titles,
+                       'year': years,
+                       'time_minute': times,
+                       'imdb_rating': imdb_ratings,
+                       'imdb_voters': votes,
+                       'metascore': metascores,
+                       'reviews': reviews,
+                       'critics': critics,
+                       #'popularity': popularitys,
                        'country':countrys,
-                       'worldwide_gross':worldwide_grosses,
-                       'movie_class':movie_classes})
+                       'worldwide_gross': worldwide_grosses,
+                       'movie_class': movie_classes})
 
 movies.to_csv('imdb_top250')
